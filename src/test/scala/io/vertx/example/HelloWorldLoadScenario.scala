34d5264435a366629f6e25ab808145a1e73411ef
jochen <666jochen666>
2015-04-30 23:04:02 +0200

    initial import

---
+++ src/test/scala/io/vertx/example/HelloWorldLoadScenario.scala
@@ -0,0 +1,28 @@
+package io.vertx.example
+
+import io.gatling.core.scenario.Simulation
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+/**
+ * Created by jmader on 29.04.15.
+ */
+class HelloWorldLoadScenario extends Simulation {
+  val httpConf = http // 4
+    .baseURL("http://127.0.0.1:8087")
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .doNotTrackHeader("1")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+
+  val scn = scenario("BasicSimulation")
+    .exec(http("request_1")
+    .get("/"))
+    .pause(5)
+
+  setUp( // 11
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpConf)
+}
