9bc5449a56ebd7b768d75436b4726401b17debd1
jochen <666jochen666>
2015-05-03 23:54:19 +0200

    Switched to different docker plugin

--- src/main/java/io/vertx/example/HelloWorldMain.java
+++ src/main/java/io/vertx/example/HelloWorldMain.java
@@ -1,7 +1,12 @@
 package io.vertx.example;
 
+import io.vertx.core.Vertx;
+
 /**
  * Created by jmader on 03.05.15.
  */
 public class HelloWorldMain {
+    public static void main(String[] args) {
+        Vertx.vertx().deployVerticle(HelloWorldVerticle.class.getName());
+    }
 }
