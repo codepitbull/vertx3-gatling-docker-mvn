96bb38df3af56b23e84a4a6349e853c253915f2c
jochen <666jochen666>
2015-05-04 23:22:47 +0200

    vagrant update

--- src/main/vagrant/createuser.sh
+++ src/main/vagrant/createuser.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/expect
+# see https://www.vultr.com/docs/setup-your-own-docker-registry-on-coreos
 set command "/usr/bin/docker login 127.0.0.1:5000"
 spawn {*}$command
 expect "Username: " { send "test\r" }
