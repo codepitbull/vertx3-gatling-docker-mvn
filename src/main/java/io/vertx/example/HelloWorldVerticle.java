7977a2bf62ee6e8cd757f8c4707866de260b7d87
jochen <666jochen666>
2015-06-23 21:25:17 +0200

    added vertx-web

--- src/main/java/io/vertx/example/HelloWorldVerticle.java
+++ src/main/java/io/vertx/example/HelloWorldVerticle.java
@@ -1,7 +1,11 @@
 package io.vertx.example;
 
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 /**
  * @author <a href="http://tfox.org">Tim Fox</a>
@@ -10,7 +14,31 @@ public class HelloWorldVerticle extends AbstractVerticle {
 
   @Override
   public void start() throws Exception {
-    // Create an HTTP server which simply returns "Hello World!" to each request.
-    Vertx.vertx().createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(8087);
+    HttpServer httpServer = vertx.createHttpServer();
+
+    Router router = Router.router(vertx);
+
+    router.get("/").handler(routingContext -> {
+      HttpServerResponse response = routingContext.response();
+      response.putHeader("content-type", "text/plain");
+      response.end("Hello World!");
+    });
+
+    router.get("/hello2").handler(routingContext -> {
+      HttpServerResponse response = routingContext.response();
+      response.putHeader("content-type", "text/plain");
+      response.end("Hello World from Vert.x-Web!");
+    });
+
+    router.post("/postme").handler(routingContext -> {
+      HttpServerResponse response = routingContext.response();
+      if(routingContext.request().formAttributes().contains("saveme")) {
+        response.end(routingContext.request().formAttributes().get("saveme"));
+        return;
+      }
+      response.setStatusCode(HttpResponseStatus.BAD_REQUEST.code()).end("FAIIILLLL!!");
+    });
+
+    httpServer.requestHandler(router::accept).listen(8087);
   }
 }
